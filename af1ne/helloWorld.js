b33e7e929d26ae851bfa0ed146214b75558dd174
Af1ne <delphine.brunet@gmail.com>
2018-10-17 19:25:45 +0200

    adding HelloWorld in JS

---
+++ af1ne/helloWorld.js
@@ -0,0 +1,13 @@
+const h = "H";
+const e = "e";
+const l = "L";
+const o = "O";
+const space = " ";
+const w = "w";
+
+const r = "r";
+
+const d = "d";
+
+const hW = h + e + l + l + o + space + w + o + r + l + d
+console.log(hW)
